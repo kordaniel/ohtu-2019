69fa4d8055c87b9e35695b21a9342fe5c661e830
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-11-08 13:22:33 +0200

    System.in configured

--- viikko2/tehtavat1-4/1-gradlen-perusteita/src/main/java/Main.java
+++ viikko2/tehtavat1-4/1-gradlen-perusteita/src/main/java/Main.java
@@ -1,5 +1,12 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-      System.out.println("Hello gradle!");
+      Scanner scanner = new Scanner(System.in);
+
+      System.out.println("Kuka olet: ");
+      String nimi = scanner.nextLine();
+
+      System.out.println("Hello " + nimi);
     }
 }
