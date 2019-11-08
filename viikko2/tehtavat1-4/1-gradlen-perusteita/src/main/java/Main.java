f1d5772b87c81a9fd60ce501f454b796c15f36a6
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-11-08 13:24:57 +0200

    multiplier implemented

--- viikko2/tehtavat1-4/1-gradlen-perusteita/src/main/java/Main.java
+++ viikko2/tehtavat1-4/1-gradlen-perusteita/src/main/java/Main.java
@@ -1,12 +1,15 @@
 import java.util.*;
+import ohtu.*;
 
 public class Main {
     public static void main(String[] args) {
       Scanner scanner = new Scanner(System.in);
 
-      System.out.println("Kuka olet: ");
-      String nimi = scanner.nextLine();
+      Multiplier kolme = new Multiplier(3);
 
-      System.out.println("Hello " + nimi);
+      System.out.println("Anna luku: ");
+      int luku = scanner.nextInt();
+
+      System.out.println("luku kertaa kolme on " + kolme.multipliedBy(luku));
     }
 }
