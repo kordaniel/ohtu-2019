8f924894c79181eb93f3e1085b3f989be5586e7f
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-11-16 20:51:28 +0200

    exercise2 complete. sorted players of country can be printed nicely formatted

--- viikko3/tehtavat1-4/nhlreader/src/main/java/ohtu/Main.java
+++ viikko3/tehtavat1-4/nhlreader/src/main/java/ohtu/Main.java
@@ -5,10 +5,58 @@
  */
 package ohtu;
 
+import com.google.gson.Gson;
+import org.apache.http.client.fluent.Request;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.GregorianCalendar;
+
+
 /**
  *
  * @author mluukkai
  */
 public class Main {
+    public static void main(String[] args) throws IOException {
+        String url = "https://nhlstatisticsforohtu.herokuapp.com/players";
+        String bodyText = Request.Get(url).execute().returnContent().asString();
+        
+        //System.out.println("json-muotoinen data:");
+        //System.out.println(bodyText);
+        
+        Gson mapper = new Gson();
+        Player[] players = mapper.fromJson(bodyText, Player[].class);
+        
+        printSortedPlayersFromCountry(players, "FIN");
+        //System.out.println("Oliot:");
+        //for (Player player : players) {
+        //    if (player.getNationality().toLowerCase().equals("fin")) {
+        //        System.out.println(player);
+        //    }
+        //}
+        
+        
+    }
+    
+    static void printSortedPlayersFromCountry(Player[] players, String country) {
+        System.out.println("Players from " + country + " " + new GregorianCalendar().getTime());
+        System.out.println("");
+        
+        Arrays.stream(players)
+                .filter(p -> p.getNationality().toLowerCase().equals(country.toLowerCase()))
+                .sorted((p1,p2) -> p2.getTotalScore() - p1.getTotalScore())                
+                .forEach(p -> {
+                    
+                    String playerStr = "%-" + 25 + "s %3s %2d + %2d = %3d%n";
+                    System.out.format(
+                            playerStr,
+                            p.getName(),
+                            p.getTeam(),
+                            p.getGoals(),
+                            p.getAssists(),
+                            p.getTotalScore()
+                    );
+                });
+    }
     
 }
--- viikko3/tehtavat1-4/nhlreader/src/main/java/ohtu/Player.java
+++ viikko3/tehtavat1-4/nhlreader/src/main/java/ohtu/Player.java
@@ -3,7 +3,61 @@ package ohtu;
 
 public class Player {
     private String name;
+    private String team;
+    private String nationality;
+    private String birthdate;
+    private int goals;
+    private int assists;
+    private int penalties;
 
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public int getPenalties() {
+        return penalties;
+    }
+
+    public void setPenalties(int penalties) {
+        this.penalties = penalties;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+    
+    public int getGoals() {
+        return this.goals;
+    }
+    
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+    
+    public int getAssists() {
+        return assists;
+    }
+    
+    public void setTeam(String team) {
+        this.team = team;
+    }
+    
+    public String getTeam() {
+        return team;
+    }
+    
     public void setName(String name) {
         this.name = name;
     }
@@ -11,10 +65,14 @@ public class Player {
     public String getName() {
         return name;
     }
+    
+    public int getTotalScore() {
+        return getGoals() + getAssists();
+    }
 
     @Override
     public String toString() {
-        return name;
+        return name + " team " + team + " goals " + goals + " assists " + assists;
     }
       
 }
