744422c6e590207251482a9e63c43f4036a14308
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-11-17 15:48:45 +0200

    Exercise9 completed. Created better formatted test for nonexisting user login using cucumber+selenium

--- viikko3/tehtavat7-11/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ viikko3/tehtavat7-11/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -12,8 +12,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class Stepdefs {
-    //WebDriver driver = new ChromeDriver();
-    WebDriver driver = new HtmlUnitDriver();
+    WebDriver driver = new ChromeDriver();
+    //WebDriver driver = new HtmlUnitDriver();
     String baseUrl = "http://localhost:4567";
     
     @Given("login is selected")
@@ -44,6 +44,7 @@ public class Stepdefs {
         pageHasContent("Give your credentials to login");
     }    
     
+    /* antipattern = bad methods
     @When("username {string} and password {string} are given")
     public void usernameAndPasswordAreGiven(String username, String password) throws Throwable {
         logInWith(username, password);
@@ -52,6 +53,11 @@ public class Stepdefs {
     @Then("system will respond {string}")
     public void systemWillRespond(String pageContent) throws Throwable {
         assertTrue(driver.getPageSource().contains(pageContent));
+    } */
+
+    @When("nonexistent username {string} and password {string} are given")
+    public void nonexistentUsernameAndPasswordAreGiven(String username, String password) {
+        logInWith(username, password);
     }
     
     @After
